1f33b14dcaf668e1922204222516eae2883f751b
DiilaNa <liyanaarachchidilan@gmail.com>
2025-07-15 11:18:08 +0530

    Save Done

--- Back_End/src/main/java/lk/ijse/project/back_end/service/impl/JobServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/project/back_end/service/impl/JobServiceImpl.java
@@ -1,9 +1,11 @@
 package lk.ijse.project.back_end.service.impl;
 
 import lk.ijse.project.back_end.dto.JobDto;
+import lk.ijse.project.back_end.entity.Job;
 import lk.ijse.project.back_end.repostory.JobRepository;
 import lk.ijse.project.back_end.service.JobService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +13,10 @@ import org.springframework.stereotype.Service;
 public class JobServiceImpl implements JobService {
 
     private final JobRepository jobRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public void saveJob(JobDto jobDto) {
-        jobRepository.save(jobDto);
+        jobRepository.save(modelMapper.map(jobDto, Job.class));
     }
 }
