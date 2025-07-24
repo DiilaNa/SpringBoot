96eb01402a01329f9a52f2ed9114dde0e37bd525
DiilaNa <liyanaarachchidilan@gmail.com>
2025-07-24 10:36:08 +0530

    Add Global Exception Handler

--- Back_End/src/main/java/lk/ijse/project/back_end/Exception/GlobalExceptionHandler.java
+++ Back_End/src/main/java/lk/ijse/project/back_end/Exception/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package lk.ijse.project.back_end.Exception;
 
+import lk.ijse.project.back_end.util.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> HandleGenericException(Exception e){
+        return new ResponseEntity<>(new ApiResponse(500,e.getMessage(),null),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
